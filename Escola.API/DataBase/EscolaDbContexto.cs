f845e45166f7fe3bbce3937943e972b29206dfe3
Beatriz Ceschini <biaceschini@gmail.com>
2023-07-12 10:59:52 -0300

    [M3S01] Ex 2 - Criando Banco de dados a partir das Models (Code First)

--- Escola.API/DataBase/EscolaDbContexto.cs
+++ Escola.API/DataBase/EscolaDbContexto.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Escola.API.Model;
 
+
 namespace Escola.API.DataBase
 {
     public class EscolaDbContexto : DbContext
@@ -26,7 +27,7 @@ namespace Escola.API.DataBase
                                         .HasName("Pk_aluno_id");
 
             modelBuilder.Entity<Aluno>().Property(x => x.Id)
-                                        .HasColumnName("PK_ID" )
+                                        .HasColumnName("PK_ID")
                                         .HasColumnType("INT");
 
             modelBuilder.Entity<Aluno>().Property(x => x.Nome)
@@ -41,7 +42,8 @@ namespace Escola.API.DataBase
                                         .HasColumnType("VARCHAR")
                                         .HasMaxLength(150);
 
-            modelBuilder.Entity<Aluno>().Property(x => x.Idade);
+            modelBuilder.Entity<Aluno>().Property(x => x.Idade)
+                                        .HasDefaultValue(null);
 
             modelBuilder.Entity<Aluno>().Property(x => x.Email)
                                         .IsRequired()
@@ -90,6 +92,72 @@ namespace Escola.API.DataBase
 
             modelBuilder.Entity<Turma>().HasIndex(x => x.Nome)
                                         .IsUnique();
+
+            modelBuilder.Entity<Materia>().ToTable("Materias");
+
+            modelBuilder.Entity<Materia>().HasKey(x => x.Id);
+
+            modelBuilder.Entity<Materia>().Property(x => x.Id)
+                                        .HasColumnName("ID")
+                                        .HasColumnType("INT");
+
+            modelBuilder.Entity<Materia>().Property(x => x.Nome)
+                                        .IsRequired()
+                                        .HasColumnName("NOME")
+                                        .HasColumnType("VARCHAR")
+                                        .HasMaxLength(50);
+
+            modelBuilder.Entity<Boletim>().ToTable("Boletins");
+
+            modelBuilder.Entity<Boletim>().Property(x => x.Id)
+                                        .HasColumnName("ID")
+                                        .HasColumnType("INT");
+
+            modelBuilder.Entity<Boletim>().HasKey(x => x.Id);
+
+            modelBuilder.Entity<Boletim>().Property(x => x.DateTime)
+                                        .IsRequired()
+                                        .HasColumnName("DATA")
+                                        .HasColumnType("DATE");
+
+            modelBuilder.Entity<Boletim>().Property(x => x.AlunoId)
+                                        .IsRequired()
+                                        .HasColumnName("Aluno_FK")
+                                        .HasColumnType("INT");
+
+            modelBuilder.Entity<Boletim>().HasOne(x => x.Aluno)
+                                        .WithMany(x => x.Boletins)
+                                        .HasForeignKey(x => x.AlunoId)
+                                        .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<NotasMateria>().ToTable("NotasMaterias");
+
+            modelBuilder.Entity<NotasMateria>().Property(x => x.Id)
+                                        .HasColumnName("ID")
+                                        .HasColumnType("INT");
+
+            modelBuilder.Entity<NotasMateria>().HasKey(x => x.Id);
+
+            modelBuilder.Entity<NotasMateria>().Property(x => x.BoletimId)
+                                        .IsRequired()
+                                        .HasColumnName("Boletim_ID")
+                                        .HasColumnType("INT");
+
+            modelBuilder.Entity<NotasMateria>().HasOne(x => x.Boletim)
+                                        .WithMany(x => x.NotasMaterias)
+                                        .HasForeignKey(x => x.BoletimId)
+                                        .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<NotasMateria>().Property(x => x.MateriaId)
+                                        .IsRequired()
+                                        .HasColumnName("Materia_ID")
+                                        .HasColumnType("INT");
+
+            modelBuilder.Entity<NotasMateria>().HasOne(x => x.Materia)
+                                        .WithMany(x => x.NotasMaterias)
+                                        .HasForeignKey(x => x.MateriaId)
+                                        .OnDelete(DeleteBehavior.Cascade);
+
 
         }
     }
---
+++ Escola.API/Migrations/20230712135300_dbCreate.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Escola.API.Migrations
+{
+    public partial class dbCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AlunoTB",
+                columns: table => new
+                {
+                    PK_ID = table.Column<int>(type: "INT", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NOME = table.Column<string>(type: "VARCHAR(50)", maxLength: 50, nullable: false),
+                    SOBRENOME = table.Column<string>(type: "VARCHAR(150)", maxLength: 150, nullable: false),
+                    Idade = table.Column<int>(type: "int", nullable: false),
+                    GENERO = table.Column<string>(type: "VARCHAR(20)", maxLength: 20, nullable: true),
+                    TELEFONE = table.Column<string>(type: "VARCHAR(30)", maxLength: 30, nullable: true),
+                    EMAIL = table.Column<string>(type: "VARCHAR(50)", maxLength: 50, nullable: false),
+                    DATA_NASCIMENTO = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("Pk_aluno_id", x => x.PK_ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Materias",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "INT", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NOME = table.Column<string>(type: "VARCHAR(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Materias", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TURMA",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CURSO = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: true, defaultValue: "Curso Basico"),
+                    Nome = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TURMA", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Boletins",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "INT", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Aluno_FK = table.Column<int>(type: "INT", nullable: false),
+                    DATA = table.Column<DateTime>(type: "DATE", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Boletins", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Boletins_AlunoTB_Aluno_FK",
+                        column: x => x.Aluno_FK,
+                        principalTable: "AlunoTB",
+                        principalColumn: "PK_ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "NotasMaterias",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "INT", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Boletim_ID = table.Column<int>(type: "INT", nullable: false),
+                    Materia_ID = table.Column<int>(type: "INT", nullable: false),
+                    Nota = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NotasMaterias", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_NotasMaterias_Boletins_Boletim_ID",
+                        column: x => x.Boletim_ID,
+                        principalTable: "Boletins",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_NotasMaterias_Materias_Materia_ID",
+                        column: x => x.Materia_ID,
+                        principalTable: "Materias",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AlunoTB_EMAIL",
+                table: "AlunoTB",
+                column: "EMAIL",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Boletins_Aluno_FK",
+                table: "Boletins",
+                column: "Aluno_FK");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_NotasMaterias_Boletim_ID",
+                table: "NotasMaterias",
+                column: "Boletim_ID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_NotasMaterias_Materia_ID",
+                table: "NotasMaterias",
+                column: "Materia_ID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TURMA_Nome",
+                table: "TURMA",
+                column: "Nome",
+                unique: true,
+                filter: "[Nome] IS NOT NULL");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "NotasMaterias");
+
+            migrationBuilder.DropTable(
+                name: "TURMA");
+
+            migrationBuilder.DropTable(
+                name: "Boletins");
+
+            migrationBuilder.DropTable(
+                name: "Materias");
+
+            migrationBuilder.DropTable(
+                name: "AlunoTB");
+        }
+    }
+}
